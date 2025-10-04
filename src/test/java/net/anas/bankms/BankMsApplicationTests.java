ce300d8f3560a640d6f99e578355968abf02d716
Anas MZAATER <anasmzaater@gmail.com>
2025-10-04 22:35:22 +0100

    first commit

---
+++ src/test/java/net/anas/bankms/BankMsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.anas.bankms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
