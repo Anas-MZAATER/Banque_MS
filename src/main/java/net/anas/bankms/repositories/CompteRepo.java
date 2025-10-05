5801d042d9f596d49e08e2ee240ec0c0bb7f6d9d
Anas MZAATER <anasmzaater@gmail.com>
2025-10-05 14:28:33 +0100

    Implement REST API using 'Spring Data REST'

--- src/main/java/net/anas/bankms/repositories/CompteRepo.java
+++ src/main/java/net/anas/bankms/repositories/CompteRepo.java
@@ -2,6 +2,9 @@ package net.anas.bankms.repositories;
 
 import net.anas.bankms.entities.Compte;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+@RepositoryRestResource
 public interface CompteRepo extends JpaRepository<Compte,Long> {
 }
