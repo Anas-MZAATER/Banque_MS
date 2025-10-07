326075ba65771d44b46b1b09da12e4eed376540f
Anas MZAATER <anasmzaater@gmail.com>
2025-10-07 10:14:33 +0100

    ajouter une methode de recherche via 'spring data rest'

--- src/main/java/net/anas/bankms/repositories/CompteRepo.java
+++ src/main/java/net/anas/bankms/repositories/CompteRepo.java
@@ -1,10 +1,17 @@
 package net.anas.bankms.repositories;
 
 import net.anas.bankms.entities.Compte;
+import net.anas.bankms.entities.TypeCompte;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 
 @RepositoryRestResource
 public interface CompteRepo extends JpaRepository<Compte,Long> {
+    @RestResource(path = "/byType")
+    public List<Compte> findByType(@Param("t") TypeCompte type);
 }
